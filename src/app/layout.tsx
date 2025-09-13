bbede369bdf8b62f77f3e04b0a8a1edb07bb511f
SviatoslavPotapenko <Sviatoslav.Potapenko@lnu.edu.ua>
2025-09-13 17:12:46 +0300

    📊 Додано Facebook Pixel код до всіх сторінок
    ✅ Facebook Pixel інтеграція:
    - Додано Facebook Pixel код в layout.tsx
    - Код буде завантажуватись на всіх сторінках сайту
    - ID пікселя: 1400694474365408
    - Автоматичне відстеження PageView
    
    ✅ Технічні деталі:
    - Використано dangerouslySetInnerHTML для безпечного вставлення
    - Додано noscript fallback для браузерів без JS
    - Розміщено в <head> секції для правильного завантаження
    - Код оптимізований для Next.js
    
    ✅ Результат:
    - Facebook Pixel активний на всіх сторінках
    - Відстеження переглядів сторінок
    - Готовність для рекламних кампаній

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -148,6 +148,32 @@ export default function RootLayout({
         {/* Preconnect to external domains */}
         <link rel="preconnect" href="https://images.unsplash.com" />
         <link rel="dns-prefetch" href="https://images.unsplash.com" />
+
+        {/* Facebook Pixel Code */}
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+              !function(f,b,e,v,n,t,s)
+              {if(f.fbq)return;n=f.fbq=function(){n.callMethod?
+              n.callMethod.apply(n,arguments):n.queue.push(arguments)};
+              if(!f._fbq)f._fbq=n;n.push=n;n.loaded=!0;n.version='2.0';
+              n.queue=[];t=b.createElement(e);t.async=!0;
+              t.src=v;s=b.getElementsByTagName(e)[0];
+              s.parentNode.insertBefore(t,s)}(window,document,'script',
+              'https://connect.facebook.net/en_US/fbevents.js');
+              fbq('init', '1400694474365408'); 
+              fbq('track', 'PageView');
+            `,
+          }}
+        />
+        <noscript>
+          <img
+            height="1"
+            width="1"
+            src="https://www.facebook.com/tr?id=1400694474365408&ev=PageView&noscript=1"
+          />
+        </noscript>
+        {/* End Facebook Pixel Code */}
       </head>
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
